cb042d737ec7616a391ceb3db013829280f01fee
Marvin <marvingabler@outlook.de>
2022-10-01 00:31:26 +0200

    added random access

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import random
 
 import psutil
 import h5py
@@ -10,6 +11,10 @@ data = [os.path.join(data_path, i) for i in files]
 
 def get_ram_usage():
     print(f"Current RAM usage: {psutil.virtual_memory()[2]} GB") 
+
+def generate_random_list(num):
+    rand_list = [random.randint(0,1460) for i in range(num)]
+    return rand_list
 
 def get_dataset(index):
     f = h5py.File(data[index])
@@ -25,12 +30,21 @@ def test_slice(dset, num):
     test = dset[:num]
     get_ram_usage()
 
-def test_full_file(dset):
+def test_random_access(dset, num):
+    rand_list = generate_random_list(num)
+    for i in rand_list:
+        test = dset[i]
+    get_ram_usage()
+
+def test_full_file_iter(dset):
     for i in range(1460):
         test = dset[i]
     get_ram_usage()
 
-start = time.time()
+def test_full_file_slice(dset):
+    test = dset[:]
+    get_ram_usage()
+
 num = 100
 
 get_ram_usage()
@@ -39,11 +53,23 @@ print(f"Opening dataset")
 dset = get_dataset(0)
 get_ram_usage()
 
+start = time.time()
 test_iter(dset, num)
 print(f"Took {time.time() - start} seconds to access {num} datapoints via iteration")
 
+start = time.time()
 test_slice(dset, num)
-print(f"Took {time.time() - start} seconds to access {num} datapoints via iteration")
+print(f"Took {time.time() - start} seconds to access {num} datapoints via slice")
 
-test_full_file(dset)
-print(f"Took {time.time() - start} seconds to access full dataset of 1460 timesteps")
+start = time.time()
+test_random_access(dset, num)
+print(f"Took {time.time() - start} seconds to randomly access {num} datapoints via iteration")
+
+start = time.time()
+test_full_file_iter(dset)
+print(f"Took {time.time() - start} seconds to access full dataset of 1460 timesteps via iteration")
+
+start = time.time()
+test_full_file_slice(dset)
+print(f"Took {time.time() - start} seconds to access full dataset of 1460 timesteps via slice")
+
