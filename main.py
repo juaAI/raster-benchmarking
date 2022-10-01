49a7446547ef207f6792c27f26ca77ff35118370
Marvin <marvingabler@outlook.de>
2022-10-02 00:01:41 +0200

    added some more tests

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import random
 
 import psutil
 import h5py
+import zarr
+import xarray
 
 data_path = "data"
-files = ["2017.h5", "2017.zarr"]
+files = ["2017.h5", "2017.zarr", "compressed_2017.h5"]
 data = [os.path.join(data_path, i) for i in files] 
 
 def get_ram_usage():
@@ -16,10 +18,42 @@ def generate_random_list(num):
     rand_list = [random.randint(0,1460) for i in range(num)]
     return rand_list
 
-def get_dataset(index):
-    f = h5py.File(data[index])
+def get_h5_dataset():
+    f = h5py.File(data[0])
     dset = f["fields"]
     return dset
+
+def save_compressed_h5(dset):
+
+    f = h5py.File("data/compressed_2017.h5", "w")
+    f.create_dataset("fields", data=dset[()], compression="gzip")
+    f.close()
+
+def get_zarr_dataset(engine="zarr"):
+    if engine == "zarr":
+        f = zarr.open(data[1])
+    elif engine == "xarray":
+        f = xarray.open_zarr(data[1])
+    return f
+    
+def test_zarr_full(f):
+    keys = list(dict(f).keys())
+    needed_keys = keys[:-3]
+
+    for i in needed_keys:
+        print(i)
+        f[i][:100]
+
+def test_xarray_iter(f, num):
+
+    for i in range(num):
+        ds = f.isel(time=i).load()
+        print(ds)
+        
+def test_xarray_slice(f, num):
+
+    ds = f.isel(time=list(range(num))).load()
+
 
 def test_iter(dset, num):
     for i in range(num):
@@ -47,29 +81,42 @@ def test_full_file_slice(dset):
 
 num = 100
 
-get_ram_usage()
-print(f"Opening dataset")
 
-dset = get_dataset(0)
-get_ram_usage()
+def test_h5():
+    get_ram_usage()
+    print(f"Opening dataset")
+
+    dset = get_dataset()
+    get_ram_usage()
+
+    start = time.time()
+    test_iter(dset, num)
+    print(f"Took {time.time() - start} seconds to access {num} datapoints via iteration")
+
+    start = time.time()
+    test_slice(dset, num)
+    print(f"Took {time.time() - start} seconds to access {num} datapoints via slice")
+
+    start = time.time()
+    test_random_access(dset, num)
+    print(f"Took {time.time() - start} seconds to randomly access {num} datapoints via iteration")
+
+    start = time.time()
+    test_full_file_iter(dset)
+    print(f"Took {time.time() - start} seconds to access full dataset of 1460 timesteps via iteration")
+
+    start = time.time()
+    test_full_file_slice(dset)
+    print(f"Took {time.time() - start} seconds to access full dataset of 1460 timesteps via slice")
+
+
+def test_zarr():
+    f = get_zarr_dataset(engine="xarray")
+    test_xarray_slice(f, 100)
 
 start = time.time()
-test_iter(dset, num)
-print(f"Took {time.time() - start} seconds to access {num} datapoints via iteration")
+test_zarr()
+print(f"Took {time.time() - start} seconds")
 
-start = time.time()
-test_slice(dset, num)
-print(f"Took {time.time() - start} seconds to access {num} datapoints via slice")
-
-start = time.time()
-test_random_access(dset, num)
-print(f"Took {time.time() - start} seconds to randomly access {num} datapoints via iteration")
-
-start = time.time()
-test_full_file_iter(dset)
-print(f"Took {time.time() - start} seconds to access full dataset of 1460 timesteps via iteration")
-
-start = time.time()
-test_full_file_slice(dset)
-print(f"Took {time.time() - start} seconds to access full dataset of 1460 timesteps via slice")
-
+#dset = get_h5_dataset()
+#save_compressed_h5(dset)
